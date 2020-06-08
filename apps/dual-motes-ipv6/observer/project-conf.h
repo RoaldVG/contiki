1d50e825240866dab1bd3a2ff1bfe4744038c271
r.vanglabbeek@gmail.com <r.vanglabbeek@gmail.com>
2020-06-08 11:06:25 +0200

    added project-conf to observerd dir

---
+++ apps/dual-motes-ipv6/observer/project-conf.h
@@ -0,0 +1,10 @@
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+
+#define RF_CHANNEL 20
+#define NETSTACK_CONF_MAC nullmac_driver
+#define NETSTACK_CONF_RDC nullrdc_driver
+#define UART_CONF_ENABLE 1
+
+//#define TCPIP_CONF_ANNOTATE_TRANSMISSIONS 1
+#endif /* PROJECT_CONF_H_ */
